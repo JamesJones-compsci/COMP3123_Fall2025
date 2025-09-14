392b094f3044623df11b7e226b70ff30d24b3f4e
James Jones <jjsfitnesstoronto@gmail.com>
2025-09-14 13:28:29 -0400

    Completed Week 2 Lab

--- wk2labPractice.js
+++ wk2labPractice.js
@@ -74,7 +74,28 @@ console.log(capitalize('nodeJs'));
 * constructor.
 */
 
-// const car2 = new Car('Pontiac Firebird', 1976);
-// console.log(car2.details());
-// const sedan = new Sedan('Volvo SD', 2018, 30000);
-// console.log(sedan.info());
+class Car {
+    constructor(model, year){
+        this.model = model;
+        this.year = year;
+    }
+    details(){
+        console.log(`Model: ${this.model} Engine ${this.year}`);
+    }
+}
+
+class Sedan extends Car {
+    constructor(model, year, balance) {
+        super(model, year);
+        this.balance = balance;
+    }
+    info(){
+        console.log(`${this.model} has a balance of ${this.balance}:.2f`);
+    }
+}
+
+console.log("\n--- Exercise 6 ---");
+const car2 = new Car('Pontiac Firebird', 1976);
+console.log(car2.details());
+const sedan = new Sedan('Volvo SD', 2018, 30000);
+console.log(sedan.info());
